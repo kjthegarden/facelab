08845a452b8a3fca8721e7a6f1e7185a2976cbb8
YunSunLee <stulie@snu.ac.kr>
2018-11-09 00:28:01 +0900

    (fix)Add person 관련 구현 내용 옮겨오기

--- openCVLibrary343/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary343/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\jungw\\OneDrive\\Documents\\2018-2_workspace\\facelab\\openCVLibrary343\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\yunsun\\Desktop\\face\\facelab\\openCVLibrary343\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
