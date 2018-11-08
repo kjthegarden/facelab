9d7dbaa3f0eaf26127244c2158d643e2d279c0c0
kjthegarden <kj4569@snu.ac.kr>
2018-11-08 18:31:20 +0900

    Baisc UI added

---
+++ app/src/main/java/snu/facelab/AddPhotoActivity.java
@@ -0,0 +1,25 @@
+package snu.facelab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+
+import ch.zhaw.facerecognitionlibrary.Helpers.FileHelper;
+
+public class AddPhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_photo);
+
+        Button btn_Start = (Button)findViewById(R.id.btn_Start);
+    }
+
+}
