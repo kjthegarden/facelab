5b0fb9fb1a3a76cb2b2b238038719237711448e4
YunSunLee <stulie@snu.ac.kr>
2018-11-29 21:37:10 +0900

    (feat) sns share multiple pictures

--- app/src/main/java/snu/facelab/PersonPhotoActivity.java
+++ app/src/main/java/snu/facelab/PersonPhotoActivity.java
@@ -1,22 +1,19 @@
 package snu.facelab;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.StateListDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.NavUtils;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
 import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.ListView;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -80,6 +77,14 @@ public class PersonPhotoActivity extends AppCompatActivity {
             }
         });
 
+        FloatingActionButton btn = (FloatingActionButton)findViewById(R.id.btn_sns);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareImage();
+            }
+        });
     }
 
     @Override
@@ -91,6 +96,32 @@ public class PersonPhotoActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // 이미지 공유 함수
+    public void shareImage() {
+
+        // 사진 절대 경로 받아오기
+
+        // 여러 개의 사진 공유
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND_MULTIPLE); //전송 메소드를 호출합니다. 여러개의 사진 Intent.ACTION_SEND_MULTIPLE
+        intent.setType("image/jpg"); //jpg 이미지를 공유 하기 위해 Type을 정의합니다.
+
+        ArrayList<Uri> files = new ArrayList<Uri>();
+
+        String[] filesToSend={"/storage/emulated/0/DCIM/Camera/20181103_084637_HDR.jpg",
+                "/storage/emulated/0/DCIM/Camera/20181101_192825.jpg"};
+
+        for(String path : filesToSend /* List of the files you want to send */) {
+            File file = new File(path);
+            Uri uri = FileProvider.getUriForFile(PersonPhotoActivity.this,
+                    "snu.facelab.provider", file);
+            files.add(uri);
+        }
+
+        intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, files);
+        startActivity(Intent.createChooser(intent, "Choose")); //Activity를 이용하여 호출 합니다.
     }
 }
 
