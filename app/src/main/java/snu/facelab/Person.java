d596749908f68e78debf7f488047d13a86377889
kjthegarden <kj4569@snu.ac.kr>
2018-12-11 23:12:06 +0900

    (feat) PhotoDetail 해시태그 추가

--- app/src/main/java/snu/facelab/Person.java
+++ app/src/main/java/snu/facelab/Person.java
@@ -7,11 +7,16 @@ public class Person implements Serializable { // 자바 빈 (java Bean)
 
     String name = "";
 
-    // 생성자가 있으면 객체 생성시 편리하다
+    // 생성자
     public Person(String mainImg, String name) {
         this.mainImg = mainImg;
         this.name = name;
+        }
+
+    public Person(String name) {
+        this.name = name;
     }
+
     public Person() {}// 기존 코드와 호환을 위해서 생성자 작업시 기본생성자도 추가
 
     public String getMainImg() {
