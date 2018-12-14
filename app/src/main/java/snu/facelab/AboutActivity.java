ccf0f6993e8274971ff4917fd4d547eb6a99c32a
kjthegarden <kj4569@snu.ac.kr>
2018-12-14 16:22:58 +0900

    (UI) about 수정

--- app/src/main/java/snu/facelab/AboutActivity.java
+++ app/src/main/java/snu/facelab/AboutActivity.java
@@ -1,12 +1,29 @@
 package snu.facelab;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        // 전체화면
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // 뒤로가기 버튼
+        FloatingActionButton backBtn = findViewById(R.id.about_back);
+        backBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
