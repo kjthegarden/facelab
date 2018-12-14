b7632a02d04264c8448717c86bcb7f08f2197862
kjthegarden <kj4569@snu.ac.kr>
2018-12-14 16:00:28 +0900

    (feat) Edit Name 기능 추가

--- app/src/main/java/snu/facelab/PersonAdapter.java
+++ app/src/main/java/snu/facelab/PersonAdapter.java
@@ -1,13 +1,16 @@
 package snu.facelab;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +19,8 @@ import com.bumptech.glide.request.RequestOptions;
 
 import java.io.File;
 import java.util.ArrayList;
+
+import static snu.facelab.MainActivity.PERSON;
 
 class PersonAdapter extends BaseAdapter {
     Context context;     // 현재 화면의 제어권자
@@ -45,13 +50,14 @@ class PersonAdapter extends BaseAdapter {
     }
     @Override // 해당번째의 행에 내용을 셋팅(데이터와 레이아웃의 연결관계 정의)
     public View getView(int position, View convertView, ViewGroup parent) {
+        final Context context = parent.getContext();
         if (convertView == null)
             convertView = inf.inflate(layout, null);
 
         ImageView iv = (ImageView)convertView.findViewById(R.id.imageView1);
         TextView tvName=(TextView)convertView.findViewById(R.id.tvName);
 
-        Person m = al.get(position);
+       final  Person m = al.get(position);
 
         Uri imageUri;
         if(m.getMainImg()!=null){
@@ -67,6 +73,19 @@ class PersonAdapter extends BaseAdapter {
                 .into(iv);
 
         tvName.setText(m.name);
+
+        // Edit Name
+        AppCompatButton bt = convertView.findViewById(R.id.edit);
+        bt.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context, EditNameActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                i.putExtra(PERSON, m);
+                context.startActivity(i);
+            }
+        });
+
         return convertView;
     }
 }
